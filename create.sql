2cb3a1969b99b0a4dc52863bed3212c85e061a6f
Evie <evc776@gmail.com>
2021-11-12 18:16:40 +0000

    create tables and partial seed data

--- create.sql
+++ create.sql
@@ -1,8 +1,93 @@
 -- Create Tables
---- Drop Tables Here
 
+--- Drop Tables Here
+DROP IF EXISTS StaffAssignment;
+DROP IF EXISTS PetAssignment;
+DROP IF EXISTS Appointment
+DROP IF EXISTS Pet;
+DROP IF EXISTS Customer;
+DROP IF EXISTS Staff;
+DROP IF EXISTS Person;
 
 --- Create Tables Here
+CREATE TABLE Person (
+    person_id SERIAL PRIMARY KEY, -- is SERIAL the same as INT AUTO INCREMENT?
+    name VARCHAR(50) NOT NULL,
+    address_first_line VARCHAR(100),
+    city VARCHAR(20) DEFAULT 'Glasgow',
+    postcode VARCHAR(8), -- Could try to constrain characters, or create a new table for postcodes
+    mobile_phone CHAR(4) UNIQUE
+);
 
+CREATE TABLE Staff (
+    person_id INT PRIMARY REFERENCES Person(person_id),
+    position VARCHAR(20) NOT NULL, -- ideally should be an enum from the back end code
+    ni_number CHAR(9) NOT NULL UNIQUE,
+    salary NUMERIC NOT NULL CHECK (salary > 0),
+    start_date DATE NOT NULL
+);
+
+CREATE TABLE Customer (
+    person_id INT PRIMARY KEY REFERENCES Person(person_id),
+    registration_date DATE NOT NULL
+);
+
+CREATE TABLE Pet (
+    pet_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL DEFAULT 'Unknown',
+    belongs_to INT REFERENCES Customer(person_id),
+    date_of_birth DATE,
+    sex CHAR(1) NOT NULL CHECK (sex IN ('M', 'F'))
+);
+
+CREATE TABLE Cat (
+    pet_id INT PRIMARY KEY REFERENCES Pet(pet_id),
+    is_indoor BOOLEAN,
+    subtype VARCHAR(20)
+);
+
+CREATE TABLE Dog (
+    pet_id INT PRIMARY KEY REFERENCES Pet(pet_id),
+    pure_breed BOOLEAN,
+    breed VARCHAR(50)
+);
+
+CREATE TABLE Appointment (
+    appointment_id INT PRIMARY KEY,
+    customer_id INT REFERENCES Customer(person_id),
+    start_dt DATETIME NOT NULL,
+    end_dt DATETIME NOT NULL
+);
+
+CREATE TABLE PetAssignment (
+    pet_id INT REFERENCES Pet(pet_id),
+    appointment_id INT REFERENCES Appointment(appointment_id),
+    PRIMARY KEY(pet_id, appointment_id)
+);
+
+CREATE TABLE StaffAssignment (
+    person_id INT REFERENCES Staff(person_id),
+    appointment_id INT REFERENCES Appointment(appointment_id),
+    PRIMARY KEY(person_id, appointment_id)
+)
 
 --- Alter Tables if necessary
+
+CREATE OR REPLACE FUNCTION PreventReceptionists()
+RETURNS trigger AS
+$$
+BEGIN
+    IF (SELECT position FROM Staff WHERE NEW.person_id = staff.person_id) = 'Receptionist' THEN
+        RAISE EXCEPTION 'Meow, you cannot have receptionists assigned to an appointment';
+    END IF;
+    RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER DetectReceptionists
+BEFORE INSERT
+ON StaffAssignment
+FOR EACH ROW 
+EXECUTE PROCEDURE PreventReceptionists();
+
--- insert.sql
+++ insert.sql
@@ -1,1 +1,19 @@
 -- Insert example data into tables
+
+INSERT INTO Person VALUES
+    ('Anne', '1 Alpha Street', 'Glasgow', 'G11AA', '0111'),
+    ('Bob', '2 Beta Road', 'Glasgow', 'G22BB', '0222'),
+    ('Cathy', '3 Gamma Avenue', 'Greenock', 'PA11AA', '0333');
+
+-- Do some tests to ensure that invalid values cannot be inserted
+INSERT INTO Person VALUES
+    ('XXX', 'XXX', 'XXX', 'XX'); -- this should produce postcode error
+
+INSERT INTO Customer VALUES
+    (1, '2021-01-01');
+
+SELECT * FROM Person, Customer WHERE Person.person_id = Customer.person_id;
+
+INSERT INTO Staff VALUES
+    (3, 'Vet', 'AA111111A', 30000, '2015-06-06');
+
